42f025cbc7aad2ee1f3a0565a2a93e40ee91b0a0
s0188071student2024212 <aleksblasey@icloud.com>
2024-11-19 12:08:17 +0300

    Update calculator.js

--- 5/calculator.js
+++ 5/calculator.js
@@ -1,8 +1,11 @@
 function calculateCost() {
-    const quantity = parseInt(document.getElementById('quantity').value);
-    const productPrice = parseInt(document.getElementById('product').value);
-    const totalCost = quantity * productPrice;
+    const quantity = parseInt(document.getElementById('quantity').value); 
+    const productPrice = parseInt(document.getElementById('product').value); 
+    if (quantity <= 0) {
+        document.getElementById('result').innerHTML = 'Количество товара должно быть больше нуля.';
+        return; 
+    }
+
+    const totalCost = quantity * productPrice; 
     document.getElementById('result').innerHTML = `Стоимость заказа: ${totalCost} руб.`;
-  }
-  
-  
+}
