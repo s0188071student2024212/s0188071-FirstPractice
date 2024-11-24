beba01457c849cf06f3cd80b852303187c59f166
s0188071student2024212 <aleksblasey@icloud.com>
2024-11-24 11:29:31 +0300

    Update calculator.js

--- 5/calculator.js
+++ 5/calculator.js
@@ -1,31 +1,14 @@
-function onClick(event) {
-    event.preventDefault();  // Предотвращаем стандартное поведение, если это форма
-
-    // Получаем значения из полей ввода
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed");
+});
+function calculateCost() {
     const quantity = parseInt(document.getElementById('quantity').value); 
     const productPrice = parseInt(document.getElementById('product').value); 
-
-    // Проверка, чтобы количество было больше нуля
     if (quantity <= 0) {
         document.getElementById('result').innerHTML = 'Количество товара должно быть больше нуля.';
         return; 
     }
 
-    // Расчет стоимости заказа
     const totalCost = quantity * productPrice; 
-
-    // Выводим результат
     document.getElementById('result').innerHTML = `Стоимость заказа: ${totalCost} руб.`;
-
-    // Выводим alert
-    alert("click");
 }
-
-// Назначаем обработчик клика после загрузки DOM
-window.addEventListener('DOMContentLoaded', function () {
-    console.log("DOM fully loaded and parsed");
-
-    // Находим кнопку и добавляем обработчик события
-    const button = document.getElementById('my-button');
-    button.addEventListener('click', onClick);
-});
